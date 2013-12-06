1dd51712b631978197188b1a2e929c8aa208fab3
Stephen Hill <stephen@gatekiller.co.uk>
2013-12-06 23:01:02 +0000

    Automated update.

--- thesurvivor2299.com/index.php
+++ thesurvivor2299.com/index.php
@@ -161,16 +161,12 @@ function SliderLaunch(id) {
 
 <body style="background-color: black;">
 <div id="content">
-<span style="font-size: 40px; font-family: 'Monofonto';">REV 
-3120 PAB
-UNAT BA XVQ, VG'F ABG BIRE LRG
-VG YBBXF ONQ
-XRRC GURZ OHFL, V'YY TRG UVZ GB GUR FURYGRE<span>
+<span style="font-size: 40px; font-family: 'Monofonto';">PLEASE STAND BY<span>
 <div id="top"></div>
-<div id="vid-count"> <span class="vid-count" data-seconds="442830"></span></div>
+<div id="vid-count"> <span class="vid-count" data-seconds="439236"></span></div>
 <div id="footer"><div class="vt-counterfooter"><img src="/assets/static/dccb3cd66b8405cf10e6a937eb8b23bfa9.png"></div></div>
 </div>
-<audio src="/radio/Blacksite-0.mp3" autoplay loop>
+<audio src="/radio/0.mp3" autoplay loop>
 
 
 
