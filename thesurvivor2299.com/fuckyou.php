a84019c7a65ee64082e71622b95a466ce2e58af0
Stephen Hill <stephen@gatekiller.co.uk>
2013-12-07 06:10:28 +0000

    Automated update.

---
+++ thesurvivor2299.com/fuckyou.php
@@ -0,0 +1,40 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv=Content-type content="text/html; charset=utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=7, IE=9">
+    <meta name="description" content="12.11.13">
+     <meta name="keywords" content="The Survivor, 2299">
+	 
+<title>2299</title>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="/assets/js/bbe160d01998eaa70d6b42091f1a10bd66.js" type="text/javascript"></script>
+<link rel="stylesheet" type="text/css" href="/assets/css/6326c898e08243230d8e2bce25db212dbd.css">
+<script type="text/javascript">
+            $(document).ready(function(){
+                $(".rev").kkcountdown({
+                	dayText		: ' ',
+                	daysText 	: ' ',
+                    hoursText	: ':',
+                    minutesText	: ':',
+                    secondsText	: '',
+                    displayZeroDays : false,
+                    callback	: function() {
+					$(".rev").remove();
+                    $(".Ovaltine").append('<img src="aa.jpg">');
+					 },
+                    addClass	: ''
+                });
+            });
+        </script>
+
+</head>
+<body style="background-color: black;">
+<div id="content">
+ <span  style="font-family: 'Monofonto'; font-size: 40px;" class="rev" data-seconds="6"></span>
+<div class="Ovaltine"></div>
+<audio src="aa.mp3" autoplay loop>
+</div>
+</body>
+</html>
--- thesurvivor2299.com/index.php
+++ thesurvivor2299.com/index.php
@@ -15,7 +15,7 @@
 <div id="content">
 <span style="font-family: 'Monofonto'; font-size:40px; position: relative; top:-10px">THAT'S ALL FOLKS</span>
 <center><iframe width="420" height="215" src="//www.youtube.com/embed/-EQ6eHeBrhM" frameborder="0" allowfullscreen></iframe></center>
-<span style="font-family: 'Monofonto'; font-size:15px; position: relative; top:10px">Special thanks to /r/Fallout the most amazing community on reddit.  PSA:  YT User 'Elevated Level', Butch, Pitor,  Liam Neeson and creator of tunnelsnakesrule.com are  not responsible for this 'hoax/rickroll'. Ah, and thepropheteer.com is a fake too (Still thumbs up for this guy it looks great!).  Don't hate bethesda, hate me. That's all. I really hope you'll get a real Fallout 4 soon, guys.</span>
+<span style="font-family: 'Monofonto'; font-size:15px; position: relative; top:10px">Special thanks to /r/Fallout the most amazing community on reddit.  PSA:  YT User 'Elevated Level', Butch, Pitor,  Liam Neeson and creator of tunnelsnakesrule.com are  not responsible for this 'hoax/rickroll'. Ah, and thepropheteer.com is a fake too (Still thumbs up for this guy it looks great!).  Don't hate bethesda, hate me. That's all. I really hope you'll get a real Fallout 4 soon, guys. <a href="http://thesurvivor2299.com/fuckyou.php">PPS To real haters: if you  really wish me dead IRL, go here </a>. Otherwise, if you're pissed off/hate me, then  see you and remember  to watch VGX!.</span>
 </div>
 </body>
 </html>
