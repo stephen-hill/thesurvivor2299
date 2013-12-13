f2b777760c6463ab1c9a7e3113de2529f92f9b0c
Stephen Hill <stephen@gatekiller.co.uk>
2013-12-13 08:01:09 +0000

    Automated update.

--- thesurvivor2299.com/index.php
+++ thesurvivor2299.com/index.php
@@ -7,14 +7,14 @@
     <meta name="description" content="12.27.13">
      <meta name="keywords" content="The Survivor, 2299">
 	 
-<title>3485=1204-992=24-9823=239482=</title>
+<title>0</title>
 <link rel="stylesheet" type="text/css" href="/assets/css/6326c898e08243230d8e2bce25db212dbd.css">
 </head>
 
-<body style="background-color: black;">
+<body id="wrapper">
 <div id="content">
+<span class="maintxt">12.27.13</span>
 </div>
-
- <audio src="/radio/Beginning.mp3"  autoplay></audio>
+ <audio src="/radio/Hope.mp3"  autoplay loop></audio>
 </body>
 </html>
